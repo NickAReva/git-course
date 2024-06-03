e21b3b82138ecf3bae7f643d5254ff2b1c010bf4
Nikolay Reva <nickareva@gmail.com>
2024-06-03 22:27:33 +0400

    Add the function SayHi

--- 13.py
+++ 13.py
@@ -7,6 +7,9 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+def SayHi():
+    print('Hello from GIT!')
+
 while True:
     address = input('Enter location: ')
     if len(address) < 1: break
